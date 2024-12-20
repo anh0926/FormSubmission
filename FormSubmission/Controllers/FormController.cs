78793aa0fea674ef38c455a07b77524066b04e2f
Anh <anh.hoang@cyclone.co.nz>
2024-12-20 15:25:36 +1300

    initial commit

---
+++ FormSubmission/Controllers/FormController.cs
@@ -0,0 +1,62 @@
+﻿using FormSubmission.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace FormSubmission.Controllers
+{
+    public class FormController : Controller
+    {
+        private readonly ILogger<FormController> _logger;
+
+        public FormController(ILogger<FormController> logger)
+        { 
+            _logger = logger;
+        }
+
+        public IActionResult Index() 
+        {
+            var form = new Form();
+            return View(form);
+        }
+
+        [HttpPost]
+        public IActionResult Submit( Form data)
+        {          
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    data.FirstName = data.FirstName.Trim();
+                    data.LastName = data.LastName.Trim();
+
+                    string jsonString = JsonSerializer.Serialize(data);
+
+                    SaveToJsonFile(jsonString);
+
+                    _logger.LogInformation($"Form data submitted successfully: {jsonString}");
+
+                    TempData["Message"] = "Data has been saved successfully.";
+
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    TempData["Message"] = $"An error occurred,please try again";
+
+                    _logger.LogError($"{ex.Message} {ex.StackTrace}");
+                }
+            }
+
+            return View("Index", data);
+        }
+
+        private void SaveToJsonFile(string jsonString)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "Data.json");
+
+            System.IO.File.AppendAllText(path, jsonString + Environment.NewLine);
+
+            _logger.LogInformation($"Data has been saved to {path}");
+        }
+    }
+}
---
+++ FormSubmission/Models/Form.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormSubmission.Models
+{
+    public class Form 
+    {
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(50, ErrorMessage = "First Name cannot be longer than 50 characters.")]
+        [RegularExpression(@"^[a-zA-Z\s,'-]+$", ErrorMessage = "Invalid name.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        [StringLength(50, ErrorMessage = "Last Name cannot be longer than 50 characters.")]
+        [RegularExpression(@"^[a-zA-Z\s,'-]+$", ErrorMessage = "Invalid name.")]
+        public string LastName { get; set; }
+    }
+}
